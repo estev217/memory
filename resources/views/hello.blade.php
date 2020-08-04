86e407e4bf7558077cffcb4b215c990c69da2fdb
estev217 <estevao.fernandes@live.fr>
2020-08-04 11:58:45 +0200

    first commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+   <!-- <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="content">
+                    <div class="toobad m-b-md text-center">
+                        DOMMAGE POUR VOUS ! :'(
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <div id="#app"></div>
+
+@endsection
