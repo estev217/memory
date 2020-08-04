0f5c88942546c25cae79be4da35f4de91f0d50ec
estev217 <estevao.fernandes@live.fr>
2020-08-04 16:24:24 +0200

    relationship between user and score done

--- app/Score.php
+++ app/Score.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Score extends Model
 {
-    //
+    /**
+     * Get the user that owns the score.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
