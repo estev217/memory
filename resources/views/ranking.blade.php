da95e38658dd8effc6726772f28617a2cc78236e
estev217 <estevao.fernandes@live.fr>
2020-08-12 21:14:27 +0200

    score done in blue in scores ranking

---
+++ resources/views/ranking.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header text-center">Classement global</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                            <table class="table">
+                                    <thead>
+                                        <tr class="text-center">
+                                            <th>#</th>
+                                            <th>Score</th>
+                                            <th>Surnom</th>
+                                            <th>Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @if(count($scores) > 0)
+                                            @foreach ($scores as $score)
+                                                @if ($score->id === $lastScoreId )
+                                                    <tr class="text-center myscore">
+                                                        <td>{{ $loop->index +1 }} </td>
+                                                        <td>{{ $score->value }}</td>
+                                                        <td>{{ $score->user->name }}</td>
+                                                        <td>{{ date_format(($score->created_at), 'd/m/Y H:i' ) }}</td>
+                                                    </tr>
+                                                @else
+                                                    <tr class="text-center">
+                                                        <td>{{ $loop->index +1 }} </td>
+                                                        <td>{{ $score->value }}</td>
+                                                        <td>{{ $score->user->name }}</td>
+                                                        <td>{{ date_format(($score->created_at), 'd/m/Y H:i' ) }}</td>
+                                                    </tr>
+                                                @endif
+                                            @endforeach
+                                        @else
+                                        <tr class="text-center">
+                                            <td>Aucun score n'a été établi pour le moment.</td>
+                                        </tr>
+                                        @endif
+                                    </tbody>
+                            </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
